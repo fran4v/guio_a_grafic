605567c88c103ae439b562d1c45d98751d455005
Francesc Vila <francescvilasubias@gmail.com>
2022-07-02 10:19:31 +0200

    added button to run conversion

--- src/app.py
+++ src/app.py
@@ -3,15 +3,22 @@ from io import StringIO
 
 def run_app():
     st.write("""
-    # Placeholder title
+    # Crea un gràfic a partir d'un guió
     """)
 
-    uploaded_file = st.file_uploader('Selecciona el guió del que vols generar el gràfic', ['txt'])
+    uploaded_file = st.file_uploader("Selecciona l'arxiu .txt", ['txt'])
+        
+    result = st.empty()
+    result.button("Crea", disabled=True, key='1')
     if uploaded_file is not None:
+        result.button("Crea", on_click=convert_txt_to_graph, args=(uploaded_file,), key='2')
+
+
+def convert_txt_to_graph(txt_file):   
+    if txt_file is not None:
         #To convert to a string based IO:
-        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+        stringio = StringIO(txt_file.getvalue().decode("utf-8"))
 
         #To read file as string:
         string_data = stringio.read()
         st.write(string_data)
-    
