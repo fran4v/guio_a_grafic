cc4b45e368c0e3551508889f3b19bd68c16e8c8f
Francesc Vila <francescvilasubias@gmail.com>
2022-07-02 17:11:38 +0200

    added xlsx template

--- src/txt_conversion.py
+++ src/txt_conversion.py
@@ -3,13 +3,23 @@ import re
 def convert(string_data):
     
     takes_list = string_data.split('------------------------------')
+    num_takes = len(takes_list)
+
     actors_in_takes_list = []
 
     for take in takes_list:
         actors = detect_actors_in_take(take)
         actors_in_takes_list.append(actors)
-    print(actors_in_takes_list)
+    
+    for i in range(num_takes//50):
+        write_xlsx_page(i, actors_in_takes_list)
+    
+
+    
 
 def detect_actors_in_take(take):
-    actors = re.findall(re.escape('*')+"(.*?)"+re.escape('*'),take)
+    actors = re.findall(re.escape('*')+"(.*?)"+re.escape('*'), take)
     return list(set(actors))
+
+def write_xlsx_page(page, actors_in_takes_list):
+    
