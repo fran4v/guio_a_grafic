bbb5abdc566bc95b69f860c992db3f7d74bb47c4
Francesc Vila <francescvilasubias@gmail.com>
2022-07-04 12:08:21 +0200

    added download zip with summary and graph

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ from io import StringIO
 from rtf_conversion import convert_graph, convert_summary
 from striprtf.striprtf import rtf_to_text
 import unidecode
+from zipfile import ZipFile
 
 def run_app():
     container = st.container()
@@ -41,10 +42,29 @@ def convert_file_to_graph(file, container, project_name, include_summary):
         if include_summary:
             convert_summary(string_data)
 
+        download(project_name, container, include_summary)
+        
+
+def download(project_name, container, include_summary):
+    if not include_summary:
         with open('grafic_output.xlsx', 'rb') as output_file:
             container.download_button(
                 label='Descarrega',
                 data=output_file,
                 file_name=f'grafic {unidecode.unidecode(project_name)}.xlsx',
                 mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-            
+    else:
+        write_zip()
+        with open("output.zip", "rb") as output_file:
+            container.download_button(
+                label='Descarrega',
+                data=output_file,
+                file_name=f'{unidecode.unidecode(project_name)}.zip',
+                mime='application/zip')
+
+
+def write_zip():
+    zipObj = ZipFile("output.zip", "w")
+    zipObj.write("grafic_output.xlsx")
+    zipObj.write("summary.rtf")
+    zipObj.close()
