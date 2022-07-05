fa8a5103bead2cce117854dd64bc8927e15cbb2f
Francesc Vila <francescvilasubias@gmail.com>
2022-07-05 20:44:38 +0200

    accept .txt and .docx

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from rtf_conversion import convert_graph, convert_summary
 from striprtf.striprtf import rtf_to_text
 import unidecode
 from zipfile import ZipFile
+import docx2txt
 
 def run_app():
     container = st.container()
@@ -11,7 +12,7 @@ def run_app():
     # Crea un gràfic a partir d'un guió
     """)
 
-    uploaded_file = container.file_uploader("Selecciona l'arxiu .rtf", ['rtf'])
+    uploaded_file = container.file_uploader("Selecciona l'arxiu .rtf, .txt o .docx", ['rtf', 'txt', 'docx'])
     project_name = container.text_input('Nom del projecte')
 
     result = container.empty()
@@ -27,13 +28,18 @@ def convert_file_to_graph(file, container, project_name, include_summary):
     if file is not None:
         bytes_data = file.getvalue() # Read file as bytes
 
-        stringio = StringIO(bytes_data.decode("utf-8")) # Convert bytes to a string based IO
+        stringio = StringIO(bytes_data.decode('ISO-8859-1')) # Convert bytes to a string based IO
 
         string_data = stringio.read() # Read file as string
 
+        # Check if its a .rtf file
         if file.type == 'text/rtf':
             string_data = rtf_to_text(string_data, errors="ignore") # Convert .rtf file to clean string, ignorning special characters
         
+        # Check if its a .docx file
+        elif file.type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+            string_data = docx2txt.process(file)  # Convert to txt
+
         convert_graph(string_data, project_name)
 
         if include_summary:
